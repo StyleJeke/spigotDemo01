7fab2f74a386ab587d7a7b532f0bb68ba4c0ce2b
追梦 <1561458197@qq.com>
2024-02-19 23:29:11 +0800

    Initial commit

---
+++ src/main/java/com/chasedream/spigotdemo01/SpigotDemo01.java
@@ -0,0 +1,17 @@
+package com.chasedream.spigotdemo01;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpigotDemo01 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // 插件加载时执行
+
+    }
+
+    @Override
+    public void onDisable() {
+        // 插件卸载时执行
+    }
+}
